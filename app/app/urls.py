babfcb74bafba33008ce82f73e92e7cfaf0c163a
omeramasib <omeramasib999@gmail.com>
2022-12-27 23:15:09 +0300

    complete the user api

--- app/app/urls.py
+++ app/app/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
-    path('api/user', include('user.urls'))
+    path('api/user/', include('user.urls'))
 ]
