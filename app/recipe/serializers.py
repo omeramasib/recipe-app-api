5993e9e18b2030186b269ff24a7681727f65d82b
omeramasib <omeramasib999@gmail.com>
2023-03-04 19:38:52 +0300

    add new features

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,20 @@ Serializers for recipe APIs.
 """
 from rest_framework import serializers
 
-from core.models import (Recipe, Tag)
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredient objects."""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -17,7 +30,7 @@ class TagSerializer(serializers.ModelSerializer):
 
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
-    tags = TagSerializer(many= True, required=False)
+    tags = TagSerializer(many=True, required=False)
 
     class Meta:
         model = Recipe
